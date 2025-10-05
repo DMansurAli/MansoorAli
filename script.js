4831912a09ebe2ebca955f5a964c8fee151a6ca6
Mansur Ali <93607104+DMansurAli@users.noreply.github.com>
2025-10-05 18:03:05 +0500

    exp and education changes

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 async function loadData() {
-  alert('hi')
-  // Load all JSONs
-  const [studentsRes, teachersRes, experienceRes] = await Promise.all([  
-    fetch('data/experience.json')
-  ]);
+  try {
+    const res = await fetch('./data/experience.json');
+    if (!res.ok) throw new Error('File not found');
+    const experiences = await res.json();
+    console.log(experiences);
 
-  const [ experiences] = await Promise.all([
-    experienceRes.json()
-  ]);
-
-  // Populate Experience
-  const expContainer = document.getElementById('experience-timeline');
-  experiences.forEach(exp => {
-    console.log(exp.company)
-    const block = document.createElement('div');
-    block.className = 'timeline__block';
-    block.innerHTML = `
-      <div class="timeline__bullet"></div>
-      <div class="timeline__header">
-        <h4 class="timeline__title">${exp.company}</h4>
-        <h5 class="timeline__meta">${exp.role}</h5>
-        <p class="timeline__timeframe">${exp.startDate} - ${exp.endDate}</p>
-      </div>
-      <div class="timeline__desc">
-        <p>${exp.descriptions}</p>
-      </div>
-    `;
-    expContainer.appendChild(block);
-  });
+    const expContainer = document.getElementById('experience-timeline');
+    experiences.forEach(exp => {
+      const block = document.createElement('div');
+      block.className = 'timeline__block';
+      block.innerHTML = `
+        <div class="timeline__bullet"></div>
+        <div class="timeline__header">
+          <h4 class="timeline__title">${exp.company}</h4>
+          <h5 class="timeline__meta">${exp.role}</h5>
+          <p class="timeline__timeframe">${exp.startDate} - ${exp.endDate}</p>
+        </div>
+        <div class="timeline__desc">
+          <p>${exp.descriptions}</p>
+        </div>
+      `;
+      expContainer.appendChild(block);
+    });
+  } catch (err) {
+    console.error('Error loading experience:', err);
+  }
 }
 
 loadData();
